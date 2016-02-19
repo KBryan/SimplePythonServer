a6d2b07b75f4f0a658c7c4aeaaa9caff96c753d9
KBryan <kwame@3einteractive.com>
2016-02-19 11:24:00 -0500

    Plotting updates

--- starter.py
+++ starter.py
@@ -1,20 +1,23 @@
 # starter.py
 
-from wsgiref.util import setup_testing_defaults
-from wsgiref.simple_server import make_server
+import matplotlib.pyplot as plt
+import scipy as sp
 
+data = sp.genfromtxt("web_traffic.tsv", delimiter="\t")
 
-def simple_app(environ, start_response):
-    setup_testing_defaults(environ)
-    status = '200 OK'
-    headers = [('Content-type', 'text/plain')]
-    start_response(status, headers)
+x = data[:,0]
+y = data[:,1]
 
-    ret = ["Hello World"]
-    return ret
-
-httpd = make_server('', 8000, simple_app)
-print("Serving on port 8000....")
-httpd.serve_forever()
+# plot the x y points with dots of size 10
+plt.scatter(x, y, s=10)
+plt.title("Web traffic over the last month")
+plt.xlabel("Time")
+plt.ylabel("Hits/hour")
+plt.xticks([w*7*24 for w in range(10)],
+           ['week %i' % w for w in range(10)])
+plt.autoscale(tight=True)
+# draw a slightly opaque, dashed grid
+plt.grid(True, linestyle='-', color='0.75')
+plt.show()
 
 
